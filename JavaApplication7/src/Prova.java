b4e12d54a23cc1ff8e2163b95a5f06df48a6a670
tonyRobustelli <A.ROBUSTELLI29@studenti.unisa.it>
2016-10-06 09:24:26 +0200

    Completata la classe Main

---
+++ JavaApplication7/src/Prova.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Antonio
+ */
+public class Prova 
+{
+    public void ciao()
+    {
+        System.out.println("x");
+    }
+}
